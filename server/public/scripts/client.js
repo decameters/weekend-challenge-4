90fea3bf57eaacccd93444c97765b74e7bf02601
liz elton <eeeqqqeee@gmail.com>
2017-12-01 16:52:43 -0600

    clicks work but in the wrong way i think

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,5 +7,25 @@ app.controller('GalleryController', ['$http', function ($http){
 
     self.imgOne = './images/cloud_1.jpg';
 
-    // clickCountOne
+    self.clickCountOne = 0;
+    self.clickCountTwo = 0;
+    self.clickCountThree = 0;
+    self.clickCountFour = 0;
+
+    self.clickOne = function(){
+        self.clickCountOne ++;
+    }
+
+    self.clickTwo = function(){
+        self.clickCountTwo++;
+    }
+
+    self.clickThree = function(){
+        self.clickCountThree++;
+    }
+
+    self.clickFour = function(){
+        self.clickCountFour++;
+    }
+
 }]);

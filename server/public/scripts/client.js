bcdf165ee14f1a0bab46ba642a964acd3a71603e
liz elton <eeeqqqeee@gmail.com>
2017-12-01 16:02:14 -0600

    images working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,8 @@ app.controller('GalleryController', ['$http', function ($http){
     console.log('GalleryController has been loaded');
     var self = this;
     self.message = 'WELCOME :)';
+
+    self.imgOne = './images/cloud_1.jpg';
+
+    // clickCountOne
 }]);

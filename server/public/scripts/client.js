9a09cf86f7d8f8f4dfa627ce95186e370a1aaf74
liz elton <eeeqqqeee@gmail.com>
2017-12-03 20:03:55 -0600

    finalized styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,81 +8,44 @@ app.controller('GalleryController', ['$http', function ($http) {
     self.imageList = [
         {
             url: "./images/scout4.jpg",
-            description: "scout",
+            description: "SCOUT",
             alt: "scout",
             visable: true
         },
         {
             url: "./images/dfj4.jpg",
-            description: "DFJ and me",
+            description: "DFJ AND ME",
             alt: "dfj",
             visable: true
         },
         {
             url: "./images/max3.jpg",
-            description: "max",
+            description: "MAX",
             alt: "max",
             visable: true
         },
         {
             url: "./images/kf_riotfest2.jpg",
-            description: "kitten forever",
+            description: "KITTEN FOREVER",
             alt: "kf",
             visable: true
         },
         {
             url: "./images/weewee.jpg",
-            description: "wee wee",
+            description: "WEE WEE",
             alt: "weewee",
             visable: true            
         },
         {
             url: "./images/amalia3.jpg",
-            description: "amalia & i",
+            description: "AMALIA & I",
             alt: "amalia",
             visable: true
         }
     ];
 
-    // self.showDescription = false;
-
     self.showDescription = function (imageClicked){
-        console.log('in show description, image clicked was', imageClicked.description);
         imageClicked.visable = imageClicked.visable ? false : true;
-
-        console.log('isVisable is', imageClicked.visable);
     };
-
-    // self.clickCount = function(){
-    //     self.clickCounter = 0;
-    //     self.clickCounter ++;
-    // }
-
-
-    
-
-    self.clickOne = function () {
-        self.clickCountOne++;
-    }
-
-    self.clickTwo = function () {
-        self.clickCountTwo++;
-    }
-
-    self.clickThree = function () {
-        self.clickCountThree++;
-    }
-
-    self.clickFour = function () {
-        self.clickCountFour++;
-    }
-
-    self.clickFive = function () {
-        self.clickCountFive++;
-    }
-
-    self.clickSix = function () {
-        self.clickCountSix++;
-    }
 
 }]);

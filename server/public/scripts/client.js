4b2562628db8ecf3e40f2d3ae770598254b427db
liz elton <eeeqqqeee@gmail.com>
2017-12-03 18:59:46 -0600

    technically working, not beautiful

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,87 @@
 var app = angular.module('LizLifeApp', []);
 
-app.controller('GalleryController', ['$http', function ($http){
+app.controller('GalleryController', ['$http', function ($http) {
     console.log('GalleryController has been loaded');
     var self = this;
     self.message = 'WELCOME :)';
 
-    self.imgOne = './images/cloud_1.jpg';
+    self.imageList = [
+        {
+            url: "./images/scout4.jpg",
+            description: "scout",
+            alt: "scout",
+            visable: true
+        },
+        {
+            url: "./images/dfj4.jpg",
+            description: "DFJ and me",
+            alt: "dfj",
+            visable: true
+        },
+        {
+            url: "./images/max3.jpg",
+            description: "max",
+            alt: "max",
+            visable: true
+        },
+        {
+            url: "./images/kf_riotfest2.jpg",
+            description: "kitten forever",
+            alt: "kf",
+            visable: true
+        },
+        {
+            url: "./images/weewee.jpg",
+            description: "wee wee",
+            alt: "weewee",
+            visable: true            
+        },
+        {
+            url: "./images/amalia3.jpg",
+            description: "amalia & i",
+            alt: "amalia",
+            visable: true
+        }
+    ];
 
-    self.clickCountOne = 0;
-    self.clickCountTwo = 0;
-    self.clickCountThree = 0;
-    self.clickCountFour = 0;
-    self.clickCountFive = 0;
-    self.clickCountSix = 0;
+    // self.showDescription = false;
 
-    self.clickOne = function(){
-        self.clickCountOne ++;
+    self.showDescription = function (imageClicked){
+        console.log('in show description, image clicked was', imageClicked.description);
+        imageClicked.visable = imageClicked.visable ? false : true;
+
+        console.log('isVisable is', imageClicked.visable);
+    };
+
+    // self.clickCount = function(){
+    //     self.clickCounter = 0;
+    //     self.clickCounter ++;
+    // }
+
+
+    
+
+    self.clickOne = function () {
+        self.clickCountOne++;
     }
 
-    self.clickTwo = function(){
+    self.clickTwo = function () {
         self.clickCountTwo++;
     }
 
-    self.clickThree = function(){
+    self.clickThree = function () {
         self.clickCountThree++;
     }
 
-    self.clickFour = function(){
+    self.clickFour = function () {
         self.clickCountFour++;
     }
 
-    self.clickFive = function(){
+    self.clickFive = function () {
         self.clickCountFive++;
     }
 
-    self.clickSix = function(){
+    self.clickSix = function () {
         self.clickCountSix++;
     }
 
